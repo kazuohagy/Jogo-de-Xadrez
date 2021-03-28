91664f28fa1d5ebc90d2941b3a43ff8c088a572c
Japaranha <kazucalado@gmail.com>
2021-03-28 12:21:24 -0300

    Placing pieces on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 
 public class ChessPiece extends Piece{
 	private Color color;
-	public ChessPiece(Board board,Color colot) {
+	public ChessPiece(Board board,Color color) {
 		super(board);
 		this.color = color;
 		// TODO Auto-generated constructor stub
