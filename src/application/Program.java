8b6b5304e407869cf8ec1975137ed96002465f08
Japaranha <kazucalado@gmail.com>
2021-03-30 16:05:10 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,11 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
+				boolean[][] possibleMoves = chessMatch.possibleMovies(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
--- src/application/UI.java
+++ src/application/UI.java
@@ -50,22 +50,35 @@ public class UI {
 		for (int i =0;pieces.length > i;i++) {
 			System.out.print((8 -i) + " ");
 			for(int j =0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j],false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i =0;pieces.length > i;i++) {
+			System.out.print((8 -i) + " ");
+			for(int j =0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print(ANSI_CYAN_BACKGROUND);
+		}
 		if(piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		}
 		else {
 			if(piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+				System.out.print(ANSI_GREEN  + piece + ANSI_RESET);
 			}
 			else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+				System.out.print(ANSI_RED + piece + ANSI_RESET);
 			}
 		}
 		System.out.print(" ");
